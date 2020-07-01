2eb1ae138be1d1d447bfa8af18f2955bff7d4f1c
tsutomu-noguchi <50165379+tsutomu-noguchi@users.noreply.github.com>
2020-07-01 17:12:09 +0900

    カスタムフックでデータフェッチ

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function useGiphy(query) {
+	const [results, setResults] = useState([]);
+	const [loading, setLoading] = useState(false);
+
+	useEffect(() => {
+		async function fetchData() {
+			try {
+				setLoading(true);
+				const response = await fetch(
+					`https://api.giphy.com/v1/gifs/search?api_key=ySGo48L37OkJ0cGH1zAfrGr8yobgFMQt&q=${query}&limit=10&offset=0&rating=G&lang=en`
+				);
+				const json = await response.json();
+				console.log(json);
+
+				setResults(
+					json.data.map((item) => {
+						return item.images.preview.mp4;
+					})
+				);
+			} finally {
+				setLoading(false);
+			}
+		}
+
+		if (query !== '') {
+			fetchData();
+		}
+	}, [query]);
+
+	return [results, loading];
 }
 
-export default App;
+export default function AsyncHooks() {
+	const [search, setSearch] = useState('');
+	const [query, setQuery] = useState('');
+	const [results, loading] = useGiphy(query);
+
+	return (
+		<div>
+			<h1>Async React Hooks</h1>
+			<form
+				onSubmit={(e) => {
+					e.preventDefault();
+					setQuery(search);
+				}}
+			>
+				<input value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Search for Gifs!" />
+				<button type="submit">Search</button>
+			</form>
+			<br />
+			{loading ? <h1>GIVE ME GIFS</h1> : results.map((item) => <video autoPlay loop key={item} src={item} />)}
+		</div>
+	);
+}
